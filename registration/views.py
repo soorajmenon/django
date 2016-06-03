c2aef4f7ad9db0646a2a11e0eb2151df9ae24e57
Sooraj <vn.soorajmenon@gmail.con>
2016-06-03 12:53:11 +0530

    3 commit

--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+import django.views.generic
+
+# Create your views here.class Home(TemplateView):
+class Home(django.views.generic.TemplateView):
+     template_name='index.html'
 
 # Create your views here.
--- workshop/urls.py
+++ workshop/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+from registration.views import Home
 admin.autodiscover()
-
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'workshop.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+                       # Examples:
+                       # url(r'^$', 'workshop.views.home', name='home'),
+                       # url(r'^blog/', include('blog.urls')),    url(r'^admin/', include(admin.site.urls)),
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^$', Home.as_view(), name='home'),
+                       )
 
-    url(r'^admin/', include(admin.site.urls)),
-)
+
